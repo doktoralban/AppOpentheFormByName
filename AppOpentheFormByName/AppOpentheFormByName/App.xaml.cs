1fa2d2a4c7bb8ed1c00fa3540f3ff6ca8896d3c1
Aydın Türker <doktoralban@outlook.com>
2021-01-09 18:13:47 +0300

    Add project files.

---
+++ AppOpentheFormByName/AppOpentheFormByName/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppOpentheFormByName
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());//because navigate other pages from mainpage.
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppOpentheFormByName/AppOpentheFormByName/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppOpentheFormByName
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+            Button btn = (Button)sender;
+            string id = btn.StyleId;
+            Assembly assembly = GetType().GetTypeInfo().Assembly;
+            Type pageType = assembly.GetType("AppOpentheFormByName." + id);
+            Page page = (Page)Activator.CreateInstance(pageType);
+            await Navigation.PushAsync(page);
+            }
+            catch (Exception ex)
+            {
+                var s = ex.Message;
+            }
+        }
+
+
+
+
+
+    }
+}
